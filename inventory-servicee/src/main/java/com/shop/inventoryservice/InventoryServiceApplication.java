dcc3007d8266f389f6a191a3d125bf20f1b992c0
holge7 <munoz.jorge.dev@gmail.com>
2022-11-01 17:26:54 +0100

    Eureka Server Discovery

--- inventory-servicee/src/main/java/com/shop/inventoryservice/InventoryServiceApplication.java
+++ inventory-servicee/src/main/java/com/shop/inventoryservice/InventoryServiceApplication.java
@@ -2,9 +2,11 @@ package com.shop.inventoryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
--- product-servicee/src/main/java/com/shop/productservice/ProductServiceApplication.java
+++ product-servicee/src/main/java/com/shop/productservice/ProductServiceApplication.java
@@ -3,8 +3,10 @@ package com.shop.productservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableEurekaClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
